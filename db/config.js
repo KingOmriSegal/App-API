b82a0894c7dfd0684259baae275ad9824d8180db
EisenSkrt <85437983+EisenSkrt@users.noreply.github.com>
2021-06-07 14:59:23 +0300

    Add files via upload

---
+++ db/config.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+const isProduction = process.env.NODE_ENV === 'production'
+
+const connetctionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
+const pool = new Pool({
+    connetctionString: connetctionString,
+});
+
+module.exports = pool;

17a92b0ba13413465f16278b7ae3d124f37f1415
unknown <Amirsil@github.com>
2021-06-08 19:50:17 +0300

    correct image names

--- Scraper/main.py
+++ Scraper/main.py
@@ -7,19 +7,20 @@ from multiprocessing import Pool
 import os
 
 ID = 4
-
+API = "http://writing-api-intelscraping2.apps.openforce.openforce.biz"
 
 def main():
     facebook_accounts = list(filter(bool, [line.strip() for line in open("accounts.txt")]))
-
+    # for account in facebook_accounts:
+    #     crawl(account)
     with Pool(min(len(facebook_accounts), 10)) as pool:
         pool.map(crawl, facebook_accounts)
 
 
 def crawl(facebook_account):
     for post in get_all_posts(facebook_account):
+        requests.post(API, data=post)
         print(post)
-        # requests.post(f"{FILTERER_IP}/{ID}", data=post)
 
 
 def get_all_posts(facebook_account: str):
@@ -29,15 +30,13 @@ def get_all_posts(facebook_account: str):
             'username': facebook_account,
             'postDate': str(post['time']),
             'likes': post['likes'],
-            'images': [process_image(image_url) for image_url in post['images']]
+            # 'images': [process_image(image_url) for image_url in post['images']]
         }
 
 
 def process_image(image_url):
     image = requests.get(image_url).content
-    save_image(image,
-               regex.search('([^.]+$)', image_url).group(),
-               "scraped-images")
+    save_image(image, "scraped-images")
     encoded_image = base64.encodebytes(image).decode('utf-8')
 
     return encoded_image
@@ -46,14 +45,11 @@ def process_image(image_url):
 def call_counter():
     info = {"count": 1}
 
-    def save_image_inner(image_content, suffix, directory):
-        if len(suffix) != 3:
-            suffix = "png"
-
+    def save_image_inner(image_content, directory):
         if not os.path.exists(directory):
             os.makedirs(directory)
 
-        with open(f"{directory}\\shellcode{info['count']}.{suffix}", "wb") as image_file:
+        with open(f"{directory}\\shellcode{info['count']}.png", "wb") as image_file:
             image_file.write(image_content)
         info["count"] += 1
 

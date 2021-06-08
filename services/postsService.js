43c7b77864bee5988f9c8691e39497647b942f7d
EisenSkrt <85437983+EisenSkrt@users.noreply.github.com>
2021-06-08 09:16:21 +0300

    Update postsService.js

--- services/postsService.js
+++ services/postsService.js
@@ -4,7 +4,7 @@ const pool = require('../db/config');
 const addPost = async (post) => {
     const insertQuery = "SELECT * FROM posts"
     const values = [post.username, post.content, post.postDate, post.likes];
-    pool.query(insertQuery, values, (err, res) => {
+    pool.query(insertQuery, (err, res) => {
         if (err) {
             console.log(err.stack)
         }

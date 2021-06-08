7ea29cea0baa89f13ee6cbe0589fcccb6d3128cf
hackathon IDF <hack@cs.colman.ac.il>
2021-06-08 16:32:02 +0300

    (FEAT) Added posts now generate appropriate flagged word links and move the writers to suspects if needed

--- services/postsService.js
+++ services/postsService.js
@@ -1,30 +1,61 @@
 const pool = require('../db/config');
 
 
-const addPost = async (post) => {
-    const insertQuery = "INSERT INTO posts (profile, content, post_date, likes) VALUES($1, $2, $3, $4) RETURNING post_id"
+const addPost = (post) => {
+    const insertQuery = "INSERT INTO posts (profile, content, post_date, likes) VALUES($1, $2, $3, $4) RETURNING *"
     const values = [post.username, post.content, post.postDate, post.likes];
     pool.query(insertQuery, values, (err, res) => {
         if (err) {
             console.log(err.stack);
         }
         else {
-            console.log(res.rows);
-            flaggedWordsInPost(post.content);
+            flaggedWordsInPost(res.rows[0]);
         }
     });
 }
 
-const flaggedWordsInPost = async (content) => {
-    const selectQuery = "SELECT * FROM words"
-    var words 
-   
-    pool.query(selectQuery, (err, res) => {
+const flaggedWordsInPost = async (post) => {
+    try {
+        const selectWordsQuery = "SELECT * FROM words";
+        const words = (await pool.query(selectWordsQuery)).rows;
+
+        const selectProfilesQuery = "SELECT * FROM profiles WHERE fb_username = $1";
+        const values = [post.profile];
+        const postsProfile = (await pool.query(selectProfilesQuery, values)).rows[0];
+
+        words.forEach(word => {
+            if (post.content.includes(word.content)) {
+                addPostWordLink(post.post_id, word.word_id);
+                if (postsProfile.wanted_state === 0) {
+                    makeProfileSuspect(postsProfile);
+                }
+            }
+        });
+    }
+    catch (err) {
+        console.log(err.stack)
+    }
+}
+
+const addPostWordLink = (post_id, word_id) => {
+    const insertQuery = "INSERT INTO post_word_links (post_id, word_id) VALUES ($1, $2)";
+    const values = [post_id, word_id]
+
+    pool.query(insertQuery, values, (err, res) => {
         if (err) {
-            console.log(err.stack)
+            console.log(err.stack);
         }
-        else {
-            console.log(res.rows);
+    });
+}
+
+const makeProfileSuspect = (profile) => {
+    const updateQuery = "UPDATE profiles SET wanted_state = 1 WHERE prof_id = $1";
+    const values = [profile.prof_id];
+
+    console.log("TEST")
+    pool.query(updateQuery, values, (err, res) => {
+        if (err) {
+            console.log(err.stack);
         }
     });
 }

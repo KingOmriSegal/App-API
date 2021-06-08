36322f10881d843dc367270ebef52526dfbe1733
hackathon IDF <hack@cs.colman.ac.il>
2021-06-08 15:52:52 +0300

    forgot to switch db

--- ReadingAPI/dbConnection/db.js
+++ ReadingAPI/dbConnection/db.js
@@ -4,8 +4,8 @@ const pool = new Pool({
     user: "root",
     password: "qwe123",
     database: "sampledb",
-    host: "localhost",
-    port: 12345
+    host: "10.128.2.114",
+    port: 5432
 });
 
 module.exports = pool;

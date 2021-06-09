6f576a3d50c8ee42fbaaa612cc4f0da37d8131d5
unknown <Amirsil@github.com>
2021-06-09 09:36:37 +0300

    Refactor closure and correct API sendings

--- Scraper/main.py
+++ Scraper/main.py
@@ -7,19 +7,17 @@ from multiprocessing import Pool
 import os
 
 ID = 4
-API = "http://writing-api-intelscraping2.apps.openforce.openforce.biz"
+
 
 def main():
     facebook_accounts = list(filter(bool, [line.strip() for line in open("accounts.txt")]))
-    # for account in facebook_accounts:
-    #     crawl(account)
     with Pool(min(len(facebook_accounts), 10)) as pool:
         pool.map(crawl, facebook_accounts)
 
 
 def crawl(facebook_account):
     for post in get_all_posts(facebook_account):
-        requests.post(API, data=post)
+        # requests.post(API, json=post)
         print(post)
 
 
@@ -35,28 +33,34 @@ def get_all_posts(facebook_account: str):
 
 
 def process_image(image_url):
-    image = requests.get(image_url).content
-    save_image(image, "scraped-images")
-    encoded_image = base64.encodebytes(image).decode('utf-8')
+    try:
+        image = requests.get(image_url).content
+        save_image(image, "scraped-images")
+        encoded_image = base64.encodebytes(image).decode('utf-8')
 
-    return encoded_image
-
-
-def call_counter():
-    info = {"count": 1}
-
-    def save_image_inner(image_content, directory):
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-
-        with open(f"{directory}\\shellcode{info['count']}.png", "wb") as image_file:
-            image_file.write(image_content)
-        info["count"] += 1
-
-    return save_image_inner
+        return encoded_image
+    except Exception as e:
+        print(e)
+        raise e
 
 
-save_image = call_counter()
+def call_counter(func):
+    call_count = 0
+
+    def inner(*args, **kwargs):
+        nonlocal call_count
+        call_count += 1
+        return func(call_count, *args, **kwargs)
+    return inner
+
+
+@call_counter
+def save_image(call_count, image_content, directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    with open(f"{directory}\\img{call_count}.png", "wb") as image_file:
+        image_file.write(image_content)
 
 
 if __name__ == '__main__':

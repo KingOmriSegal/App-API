b205e292fe62bd63778df4f27d12211563d30fb0
hackathon IDF <hack@cs.colman.ac.il>
2021-06-09 11:52:59 +0300

    (FEAT) concatonated bad words that originate from same post

--- ReadingAPI/services/postService.js
+++ ReadingAPI/services/postService.js
@@ -19,11 +19,12 @@ exports.countPostsLastWeek = async (profileSSN) => {
 };
 
     exports.postsWithBadWords = async () => {
-            const postQuery = `SELECT profile.firstname, profile.lastname , post.content, post.post_date, word.content FROM posts AS post
-                                JOIN post_word_links AS link ON post.post_id = link.post_id 
-                                JOIN words AS word ON word.word_id = link.word_id
-                                JOIN profiles AS profile ON profile.fb_username = post.profile
-                                WHERE post_date >= (now() - '1 day'::interval)
-                                ORDER BY post_date desc`;
-            return  (await pool.query(postQuery)).rows
+            const postQuery = `SELECT profile.firstname, profile.lastname, post.post_date, STRING_AGG(word.content, ' ') AS bad_words FROM posts AS post
+            JOIN post_word_links AS link ON post.post_id = link.post_id 
+            JOIN words AS word ON word.word_id = link.word_id
+            JOIN profiles AS profile ON profile.fb_username = post.profile
+            WHERE post_date >= (now() - '1 day'::interval)
+            GROUP BY post.post_id, profile.firstname, profile.lastname
+            ORDER BY post_date desc`;
+            return (await pool.query(postQuery)).rows
     }
